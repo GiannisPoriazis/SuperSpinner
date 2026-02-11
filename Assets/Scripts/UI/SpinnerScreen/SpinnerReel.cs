250363f87d24c21c7734d0555892d765b6c77c9f
Ioannis Poriazis <ioannis.poriazis@gmail.com>
2026-02-11 15:38:52 +0200

    Creating spinner values

---
+++ Assets/Scripts/UI/SpinnerScreen/SpinnerReel.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SuperSpinner.UI
+{
+    public class SpinnerScreen: MonoBehaviour
+    {
+        private void Start()
+        {
+            foreach(var value in GameManager.SpinnerValues.Value.spinnerValues)
+            {
+                GameObject reelValue = Instantiate(Resources.Load<GameObject>("SpinnerValue"), transform);
+                reelValue.GetComponent<TextMeshProUGUI>().text = value.ToString();
+            }
+        }
+    }
+}
